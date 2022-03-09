de709df9a54d1676d6a752c31888a69abf38d286
crazytuzi <957824770@qq.com>
2022-03-09 15:44:16 +0800

    Initial Project

---
+++ Source/SlateTabDemo/SlateTabDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlateTabDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLATETABDEMO_API ASlateTabDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
